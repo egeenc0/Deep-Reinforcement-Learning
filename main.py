90ac540be9716fcb920522e75a234da85979cfc9
EgeEnc <egeenc870@gmail.com>
2024-03-24 21:36:12 +0300

    Empty Project

--- main.py
+++ main.py
@@ -1,19 +1,2 @@
 import tensorflow
 import random
-
-class Agent:
-    def __init__(self):
-        self.epsilon = 0.3
-        self.actions = []
-        self.state = 0
-
-    def act(self):
-        action = None
-        if(random.random() > self.epsilon):
-            action = random.choice(self.actions)
-            return action
-        else:
-            return self.model(self.state)
-
-    def model(self,state):
-        return None
