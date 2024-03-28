8a0af21531224e9c1a7c8a5eacfb3365ff100e6e
EgeEnc <egeenc870@gmail.com>
2024-03-28 21:04:30 +0300

    Working on the project

--- Agent.py
+++ Agent.py
@@ -1,17 +1,4 @@
 import random
 class Agent:
     def __init__(self):
-        self.epsilon = 0.3
-        self.actions = []
-        self.state = 0
-
-    def act(self):
-        action = None
-        if(random.random() > self.epsilon):
-            action = random.choice(self.actions)
-            return action
-        else:
-            return self.model(self.state)
-
-    def model(self,state):
-        return None
+        pass
--- main.py
+++ main.py
@@ -1,2 +1,29 @@
 import tensorflow
 import random
+import gym
+
+from Agent import Agent
+
+agent = Agent()
+
+R_table = [
+    [-1, 0, 0, -1],
+    [-1, 0, 0, -1],
+    [-1, 0, -1, -1],
+    [-1, 0, 0, 100]
+]
+agent.position = [0, 1]
+
+epsilon = 0.3
+condition = False
+for i in range(100):
+    if epsilon > random.random():
+        while(not condition):
+            agent.position[0] = random.choice([agent.position[0] + 1, agent.position[0] - 1,
+                                              agent.position[0]])
+            agent.position[1] = random.choice([agent.position[1] + 1, agent.position[1] - 1,
+                                              agent.position[1]])
+            condition_x = (agent.position[0] >= 0 and agent.position[0] <= 3)
+            condition_y = (agent.position[1] >= 0 and agent.position[1] <= 3)
+            condition = condition_x and condition_y
+    print("Agent's position in the maze : ",agent.position)
